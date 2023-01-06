c1fa70a2e2e8569243c606ca35b6424013a52ed5
jinzhehao <jinzhehao@souche.com>
2023-01-06 16:24:24 +0800

    fmt

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { Command } from "commander";
-
 import git from "@/command/git";
 
-const program = new Command();
-
-program
+new Command()
   .name("cli")
   .description("自用 cli 合集")
   .version("0.0.1")
-  .addCommand(git());
-
-program.parse(process.argv);
+  .addCommand(git())
+  .parse(process.argv);
