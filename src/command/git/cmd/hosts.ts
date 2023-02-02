4d2cc2032d9505b776ebf122efb99bfe82b3c994
金哲豪 <jinzhehao@souche.com>
2023-02-02 10:02:51 +0800

    edit

--- src/command/git/cmd/hosts.ts
+++ src/command/git/cmd/hosts.ts
@@ -6,32 +6,25 @@ import api from "@/api";
 export default new CliCommand({
   command: "hosts",
   description: "自动更新 hosts，需要有 /etc/hosts 的写入权限",
-  action(props) {
-    const job = props.helper.runCron({
-      cronTime: "0 0 11 * * *",
-      async onTick() {
-        const reg =
-          /^(# @@_INSERT_GIT_HOST_START_@@)([\s\S]+)(# @@_INSERT_GIT_HOST_END_@@)$/gm;
+  async action(props) {
+    const reg =
+      /^(# @@_INSERT_GIT_HOST_START_@@)([\s\S]+)(# @@_INSERT_GIT_HOST_END_@@)$/gm;
 
-        const hosts = await api.yoqi.hosts();
+    const hosts = await api.yoqi.hosts();
 
-        const str = `# @@_INSERT_GIT_HOST_START_@@\n# ${dayjs().format()}${hosts}# @@_INSERT_GIT_HOST_END_@@`;
+    const str = `# @@_INSERT_GIT_HOST_START_@@\n# ${dayjs().format()}${hosts}# @@_INSERT_GIT_HOST_END_@@`;
 
-        const oldHosts = readFileSync("/etc/hosts", { encoding: "utf-8" });
+    const oldHosts = readFileSync("/etc/hosts", { encoding: "utf-8" });
 
-        if (!reg.test(oldHosts)) {
-          props.logger.info("未发现插入 flag，insert hosts");
-          writeFileSync("/etc/hosts", `${oldHosts}\n${str}`);
-        } else {
-          props.logger.info("发现插入 flag，update hosts");
-          const newHosts = oldHosts.replace(reg, str);
-          writeFileSync("/etc/hosts", newHosts);
-        }
+    if (!reg.test(oldHosts)) {
+      props.logger.info("未发现插入 flag，insert hosts");
+      writeFileSync("/etc/hosts", `${oldHosts}\n${str}`);
+    } else {
+      props.logger.info("发现插入 flag，update hosts");
+      const newHosts = oldHosts.replace(reg, str);
+      writeFileSync("/etc/hosts", newHosts);
+    }
 
-        props.logger.info("/etc/hosts 更新成功");
-      },
-    });
-
-    job.start();
+    props.logger.info("/etc/hosts 更新成功");
   },
 });
