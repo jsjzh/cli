624c560fcfa2027d50dee5097672644ae2b919a0
金哲豪 <jinzhehao@souche.com>
2023-02-06 09:49:55 +0800

    edit

--- src/command/demo/index.ts
+++ src/command/demo/index.ts
@@ -3,13 +3,13 @@ import { CliCommand } from "@oishi/cli-core";
 export default new CliCommand({
   command: "demo",
   description: "测试指令",
-  arguments: {
-    name: {
-      description: "输入名字",
-    },
-  },
-  options: {},
-  action(props) {
-    console.log(props.data);
-  },
+  // arguments: {
+  //   name: {
+  //     description: "输入名字",
+  //   },
+  // },
+  // options: {},
+  // action(props) {
+  //   console.log(props.data);
+  // },
 });
