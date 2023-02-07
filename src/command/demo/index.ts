729abb0044bf70a50468515d8befeee30772d633
金哲豪 <jinzhehao@souche.com>
2023-02-07 09:43:16 +0800

    edit

--- src/command/demo/index.ts
+++ src/command/demo/index.ts
@@ -4,14 +4,13 @@ export default new CliCommand({
   command: "demo",
   description: "测试指令",
   arguments: {
-    name: {
-      description: "请输入名称",
-    },
-    age: {
-      description: "请输入年龄",
-    },
+    name: { description: "请输入名称" },
+    age: { description: "请输入年龄" },
   },
-  // options: {},
+  options: {
+    names: { description: "请输入名称" },
+    ages: { description: "请输入年龄" },
+  },
   action(props) {
     console.log("您输入的名称是：", props.data.name);
     console.log("您输入的年龄是：", props.data.name);
