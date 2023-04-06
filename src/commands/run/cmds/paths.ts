df47b441b6204c6e21b23eedf882b51b04baae07
jsjzh <kimimi_king@163.com>
2023-04-06 11:27:26 +0800

    feat: 修改 run paths 的使用方式

--- src/commands/run/cmds/paths.ts
+++ src/commands/run/cmds/paths.ts
@@ -1,77 +1,37 @@
-import { asyncExec, createGlobalDataTools } from "@/util";
+import { asyncExec } from "@/util";
 import { CliCommand } from "@oishi/cli-core";
-import { readdirSync } from "fs-extra";
-import path from "path";
-
-const createGitPushTagGlobalTools = (name: string) =>
-  createGlobalDataTools("run-paths", name);
-
-const globalDataRunPathsPackTools =
-  createGitPushTagGlobalTools("pathsPack.json");
-
-const arr = [
-  "amock",
-  "cli-core",
-  "cli",
-  "tiny-code-react-jsonplaceholder-app",
-  "tiny-code-web",
-  "tiny-code-react",
-  "tiny-code-react-antd-app",
-];
 
 interface IArgs {
   cmd: string;
 }
 
-export default new CliCommand<IArgs>({
+interface IOpts {
+  paths: string;
+}
+
+const splitTag = ", ";
+
+export default new CliCommand<IArgs, IOpts>({
   command: "paths",
   description: "在不同目录下执行命令",
   arguments: { cmd: { description: "输入执行命令" } },
+  options: { paths: { description: `执行命令的路径，用「${splitTag}」分割` } },
   action(props) {
-    const base = path.resolve(process.cwd());
-    const dirPaths = readdirSync(base).map((dir) => path.join(base, dir));
-
-    const currentPaths = dirPaths.filter((p) =>
-      arr.includes(path.parse(p).name),
-    );
-
-    const choices = currentPaths.map((item) => ({
-      name: path.parse(item).name,
-      value: item,
-    }));
-
-    let preData = [];
-
-    try {
-      preData = globalDataRunPathsPackTools.readJSON();
-    } catch (error) {}
-
-    props.helper
-      .runPrompt({ paths: preData[base] || [] })
-      .addCheckbox({
-        name: "paths",
-        message: "请选择执行命令的文件夹",
-        choices,
-      })
-      .execute((values) => {
-        values.paths.forEach(async (p: string) => {
-          try {
-            const result = await asyncExec(props.data.cmd!, {
-              cwd: p,
-              encoding: "utf8",
-            });
-
-            console.log(p, result.stdout);
-            console.log();
-            console.log();
-          } catch (error) {
-            console.log(error);
-            props.logger.error(error);
-            process.exit(1);
-          }
+    props.data.paths?.split(splitTag).forEach(async (p) => {
+      try {
+        const result = await asyncExec(props.data.cmd!, {
+          cwd: p,
+          encoding: "utf8",
         });
 
-        globalDataRunPathsPackTools.writeJSON({ [base]: values.paths });
-      });
+        console.log(p, result.stdout);
+        console.log();
+        console.log();
+      } catch (error) {
+        console.log(error);
+        props.logger.error(error);
+        process.exit(1);
+      }
+    });
   },
 });
