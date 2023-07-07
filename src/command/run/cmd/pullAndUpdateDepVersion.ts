3f49989338543ff83541552a549613900c70259e
jsjzh <kimimi_king@163.com>
2023-07-07 11:34:10 +0800

    chore: update dep version

--- src/command/run/cmd/pullAndUpdateDepVersion.ts
+++ src/command/run/cmd/pullAndUpdateDepVersion.ts
@@ -60,17 +60,6 @@ export default new CliCommand<IArgs, IOpts>({
       `cli run paths "cli git pull" --paths "${currPullPaths.join(", ")}"`,
     );
 
-    // const promises = currPullPaths.map((item) => {
-    //   console.log(`将在 ${item} 路径执行 cli git pull 命令`);
-
-    //   return asyncExec("cli git pull", {
-    //     cwd: item,
-    //     encoding: "utf8",
-    //   });
-    // });
-
-    // await Promise.all(promises);
-
     const currUpdateDepVersionPaths = updateDepVersionPaths.map((item) =>
       path.join(basePath, item),
     );
@@ -79,18 +68,11 @@ export default new CliCommand<IArgs, IOpts>({
       throw new Error("updateDepversion 步骤发现不能解析的路径，请检查");
     }
 
-    const promises2 = currUpdateDepVersionPaths.map((item) => {
-      console.log(
-        `将在 ${item} 路径执行 cli git push 'update dep version' --user jsjzh 命令`,
-      );
-
-      return asyncExec("cli git push 'update dep version' --user jsjzh", {
-        cwd: item,
-        encoding: "utf8",
-      });
-    });
-
-    await Promise.all(promises2);
+    await asyncExec(
+      `cli run paths "cli git push 'update dep version' --user jsjzh" --paths "${currUpdateDepVersionPaths.join(
+        ", ",
+      )}"`,
+    );
 
     console.log("执行完毕");
   },
