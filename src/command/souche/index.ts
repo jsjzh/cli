61f9ce668a7b505c3711baadce6ea53cca911634
jsjzh <kimimi_king@163.com>
2023-12-28 11:42:03 +0800

    chore: delete souche

--- src/command/souche/index.ts
+++
@@ -1,8 +0,0 @@
-import { CliCommand } from "@oishi/cli-core";
-import docker from "./cmd/docker";
-
-export default new CliCommand({
-  command: "souche",
-  description: "souche 命令合集",
-  commands: [docker],
-});
